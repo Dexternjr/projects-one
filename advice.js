9de0722f8e4332917c12c331a7a0ea362967cefc
Dexter NJr <138676917+Dexternjr@users.noreply.github.com>
2024-03-12 04:03:10 +0100

    Create advice.js

---
+++ advice.js
@@ -0,0 +1,15 @@
+class MotivationalMessage {
+    constructor() {
+        this.message = "Work hard and be consistent";
+    }
+
+    printMessage() {
+        console.log(this.message);
+    }
+}
+
+// Create an instance of the MotivationalMessage class
+const motivation = new MotivationalMessage();
+
+// Print the motivational message
+motivation.printMessage();
